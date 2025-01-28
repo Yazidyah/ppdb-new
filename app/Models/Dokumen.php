eab67e5b727053126f7df5c49d0c68e4671064f3
Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
2025-01-28 19:18:29 +0700

    FEATURE: add fillable properties to Dokumen model

--- app/Models/Dokumen.php
+++ app/Models/Dokumen.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dokumen extends Model
 {
-    //
+    protected $fillable = [
+        'id_registrasi', 
+        'id_syarat', 
+        'tipe_dokumen', 
+        'direktori_file', 
+        'uploaded_at'
+    ];
 }
