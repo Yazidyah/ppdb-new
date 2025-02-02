2c761df582fa06b238bbf856aa8e5ef0c395863f
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-02-02 15:51:32 +0700

    feat: orang tua

--- app/Livewire/OrangTuaForm.php
+++ app/Livewire/OrangTuaForm.php
@@ -9,7 +9,6 @@ use App\Models\PekerjaanOrangTua;
 
 class OrangTuaForm extends Component
 {
-    public $index;
     public $orangTua;
     public $id_hubungan, $nama_lengkap, $nik, $pekerjaan, $no_telp;
     public $hubunganOptions;
@@ -23,19 +22,61 @@ class OrangTuaForm extends Component
         'no_telp' => 'required|numeric',
     ];
 
-    public function mount($index)
+    public $messages = [
+        'nama_lengkap.required' => 'Nama Lengkap tidak boleh kosong',
+        'nik.required' => 'NIK tidak boleh kosong',
+        'nik.numeric' => 'NIK harus berupa angka',
+        'pekerjaan.required' => 'Pekerjaan tidak boleh kosong',
+        'no_telp.required' => 'No. Telp tidak boleh kosong',
+        'no_telp.numeric' => 'No. Telp harus berupa angka',
+    ];
+
+    public function mount()
     {
-        $this->index = $index;
         $this->hubunganOptions = HubunganOrangTua::orderBy('id_hubungan', 'asc')->get();
-        $this->pekerjaanOptions = PekerjaanOrangTua::all();
+        $this->pekerjaanOptions = PekerjaanOrangTua::query()->when($this->orangTua->id_hubungan != 1, function ($query) {
+            return $query->where('id_pekerjaan', '!=', 1);
+        })->orderBy('id_pekerjaan', 'asc')->get();
+        $this->nama_lengkap = $this->orangTua->nama_lengkap;
+        $this->id_hubungan = $this->orangTua->id_hubungan;
+        $this->nik = $this->orangTua->nik;
+        $this->pekerjaan = $this->orangTua->pekerjaan;
+        $this->no_telp = $this->orangTua->no_telp;
     }
     public function updatedNamaLengkap($value)
     {
-        // $this->validateOnly('nama_lengkap');
+        $this->validateOnly('nama_lengkap');
         $this->orangTua->nama_lengkap = $value;
         $this->orangTua->save();
     }
 
+    public function updatedIdHubungan($value)
+    {
+        $this->validateOnly('id_hubungan');
+        $this->orangTua->id_hubungan = $value;
+        $this->orangTua->save();
+    }
+
+    public function updatedNik($value)
+    {
+        $this->validateOnly('nik');
+        $this->orangTua->nik = $value;
+        $this->orangTua->save();
+    }
+
+    public function updatedPekerjaan($value)
+    {
+        $this->validateOnly('pekerjaan');
+        $this->orangTua->pekerjaan = $value;
+        $this->orangTua->save();
+    }
+
+    public function updatedNoTelp($value)
+    {
+        $this->validateOnly('no_telp');
+        $this->orangTua->no_telp = $value;
+        $this->orangTua->save();
+    }
 
     public function render()
     {
--- resources/views/livewire/orang-tua-form.blade.php
+++ resources/views/livewire/orang-tua-form.blade.php
@@ -1,65 +1,75 @@
 <div>
-<h1 class="font-semibold py-2 ">Informasi Orang Tua</h1>
-<div class="md:flex gap-3 w-full">
-    <div class="md:grid flex flex-col grid-cols-4 grid-rows-3 gap-4 py-2 w-full">
-        <!-- Nama Lengkap -->
-        <div class="items-center justify-center col-span-3">
-            <div
-                class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
-                <x-reg-input-text id="nama_lengkap_{{ $index }}"
-                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
-                    type="text" name="nama_lengkap" wire:model="nama_lengkap" placeholder="Nama Lengkap" />
-                <x-input-error :messages="$errors->get('nama_lengkap')" class="mt-2" />
+    <h1 class="font-semibold py-2 ">Informasi Orang Tua</h1>
+    <div class="md:flex gap-3 w-full">
+        <div class="md:grid flex flex-col grid-cols-4 grid-rows-3 gap-4 py-2 w-full">
+            <!-- Nama Lengkap -->
+            <div class="items-center justify-center col-span-3">
+                <div
+                    class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
+                    <x-reg-input-text id="nama_lengkap"
+                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
+                        type="text" name="nama_lengkap" wire:model.live="nama_lengkap" placeholder="Nama Lengkap" />
+                    @error('nama_lengkap')
+                        <span class="text-xs text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
-        </div>
-        <!-- Hubungan -->
-        <div class="items-center justify-center col-span-1">
-            <div
-                class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
-                <select id="hubungan_{{ $index }}" name="hubungan"
-                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
-                    wire:model="id_hubungan">
-                    @foreach($hubunganOptions as $option)
-                        <option value="{{ $option->id }}">{{ $option->nama_hubungan }}</option>
-                    @endforeach
-                </select>
-                <x-input-error :messages="$errors->get('id_hubungan')" class="mt-2" />
+            <!-- Hubungan -->
+            <div class="items-center justify-center col-span-1">
+                <div
+                    class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
+                    <select id="hubungan" name="hubungan"
+                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
+                        wire:model.live="id_hubungan">
+                        @foreach ($hubunganOptions as $option)
+                            <option value="{{ $option->id_hubungan }}">{{ $option->nama_hubungan }}</option>
+                        @endforeach
+                    </select>
+                    @error('id_hubungan')
+                        <span class="text-xs text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
-        </div>
-        <!-- NIK -->
-        <div class="items-center justify-center col-span-3">
-            <div
-                class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
-                <x-reg-input-text id="nik_{{ $index }}"
-                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
-                    type="tel" name="nik" wire:model="nik" placeholder="NIK" />
-                <x-input-error :messages="$errors->get('nik')" class="mt-2" />
+            <!-- NIK -->
+            <div class="items-center justify-center col-span-3">
+                <div
+                    class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
+                    <x-reg-input-text id="nik"
+                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
+                        type="tel" name="nik" wire:model.live="nik" placeholder="NIK" />
+                    @error('nik')
+                        <span class="text-xs text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
-        </div>
-        <!-- Pekerjaan -->
-        <div class="items-center justify-center col-span-3">
-            <div
-                class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
-                <select id="pekerjaan_{{ $index }}" name="pekerjaan"
-                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
-                    wire:model="pekerjaan">
-                    @foreach($pekerjaanOptions as $option)
-                        <option value="{{ $option->id_pekerjaan }}">{{ $option->nama_pekerjaan }}</option>
-                    @endforeach
-                </select>
-                <x-input-error :messages="$errors->get('pekerjaan')" class="mt-2" />
+            <!-- Pekerjaan -->
+            <div class="items-center justify-center col-span-3">
+                <div
+                    class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
+                    <select id="pekerjaan" name="pekerjaan"
+                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
+                        wire:model.live="pekerjaan">
+                        @foreach ($pekerjaanOptions as $option)
+                            <option value="{{ $option->id_pekerjaan }}">{{ $option->nama_pekerjaan }}</option>
+                        @endforeach
+                    </select>
+                    @error('pekerjaan')
+                        <span class="text-xs text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
-        </div>
-        <!-- Nomor Telepon -->
-        <div class="items-center justify-center col-span-3">
-            <div
-                class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
-                <x-reg-input-text id="no_telp_{{ $index }}"
-                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
-                    type="tel" name="no_telp" wire:model="no_telp" placeholder="Nomor Telepon" />
-                <x-input-error :messages="$errors->get('no_telp')" class="mt-2" />
+            <!-- Nomor Telepon -->
+            <div class="items-center justify-center col-span-3">
+                <div
+                    class="w-full h-full flex rounded-md shadow-sm ring-1 ring-inset ring-dasar2 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dasar2 ">
+                    <x-reg-input-text id="no_telp"
+                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
+                        type="tel" name="no_telp" wire:model.live="no_telp" placeholder="Nomor Telepon" />
+                    @error('no_telp')
+                        <span class="text-xs text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
         </div>
     </div>
-</div>
 </div>
