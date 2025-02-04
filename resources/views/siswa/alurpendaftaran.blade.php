6332ae400faaf6cd0d8402e4e666c83a0c7781a3
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-02-04 20:23:07 +0700

    fix: dark alur pendaftaran and dashboard

--- resources/views/siswa/alurpendaftaran.blade.php
+++ resources/views/siswa/alurpendaftaran.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-<div class="container mx-auto pt-5 px-4">
+    <div class="container mx-auto pt-5 px-4">
         <div class="my-4 bg-tertiary rounded-lg text-white text-center py-8 leading-tight">
             <h2 class="font-bold text-3xl md:text-4xl ">Alur Pendaftaran</h2>
             <p class="font-bold text-xl md:text-4xl ">Penerimaan Peserta Didik Baru</p>
@@ -8,87 +8,99 @@
 
     <div class="container mx-auto px-6 pt-4">
         <div class="h-full">
-    
-        <ol class="items-start sm:flex">
-    <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
-                <h1 class="text-white">1</h1>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+
+            <ol class="items-start sm:flex">
+                <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
+                    <div class="flex items-center">
+                        <div
+                            class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
+                            <h1 class="text-white">1</h1>
+                        </div>
+                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                    </div>
+                    <div class="mt-3 sm:pe-8 ">
+                        <h3 class="text-lg font-semibold text-gray-900 ">Registrasi</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
+                        <p class="text-base font-normal text-wrap text-tertiary ">Mulai registrasi dengan memasukkan
+                            NISN.</p>
+                    </div>
+                </li>
+                <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
+                    <div class="flex items-center">
+                        <div
+                            class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
+                            <h1 class="text-white">2</h1>
+                        </div>
+                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                    </div>
+                    <div class="mt-3 sm:pe-8">
+                        <h3 class="text-lg font-semibold text-gray-900 ">Isi Biodata</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
+                        <p class="text-base font-normal text-wrap text-tertiary ">Lengkapi formulir biodata. Setelah
+                            disimpan kemudian sistem akan mengirimkan pesan konfirmasi ke email yang didaftarkan.</p>
+                    </div>
+                </li>
+                <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
+                    <div class="flex items-center">
+                        <div
+                            class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
+                            <h1 class="text-white">3</h1>
+                        </div>
+                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                    </div>
+                    <div class="mt-3 sm:pe-8">
+                        <h3 class="text-lg font-semibold text-gray-900 ">Pilih Jalur</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
+                        <p class="text-base font-normal text-wrap text-tertiary ">Pilih salah satu jalur PPDB yang akan
+                            diambil, pastikan hasil scan dokumen persyaratan sudah siap.</p>
+                    </div>
+                </li>
+                <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
+                    <div class="flex items-center">
+                        <div
+                            class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
+                            <h1 class="text-white">4</h1>
+                        </div>
+                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                    </div>
+                    <div class="mt-3 sm:pe-8">
+                        <h3 class="text-lg font-semibold text-gray-900 ">Unggah Persyaratan</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
+                        <p class="text-base font-normal text-wrap text-tertiary ">Unggah dokumen persyaratan sesuai
+                            jalur yang dipilih dengan file .jpg, .jpeg atau .pdf.</p>
+                    </div>
+                </li>
+                <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
+                    <div class="flex items-center">
+                        <div
+                            class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
+                            <h1 class="text-white">5</h1>
+                        </div>
+                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                    </div>
+                    <div class="mt-3 sm:pe-8">
+                        <h3 class="text-lg font-semibold text-gray-900 ">Tes Luring</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
+                        <p class="text-base font-normal text-wrap text-tertiary ">Mengikuti tes BQ dan akademik secara
+                            offline, dengan menaati protokol kesehatan.</p>
+                    </div>
+                </li>
+                <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
+                    <div class="flex items-center">
+                        <div
+                            class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
+                            <h1 class="text-white">6</h1>
+                        </div>
+                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                    </div>
+                    <div class="mt-3 sm:pe-8">
+                        <h3 class="text-lg font-semibold text-gray-900 ">Pengumuman</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
+                        <p class="text-base font-normal text-wrap text-tertiary ">Hasil penerimaan diumumkan sesuai
+                            jadwal, melalui website ini dan melalui email yang didaftarkan.</p>
+                    </div>
+                </li>
+            </ol>
         </div>
-        <div class="mt-3 sm:pe-8 ">
-            <h3 class="text-lg font-semibold text-gray-900 ">Registrasi</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
-            <p class="text-base font-normal text-wrap text-tertiary ">Mulai registrasi dengan memasukkan NISN.</p>
-        </div>
-    </li>
-    <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
-                <h1 class="text-white">2</h1>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-        </div>
-        <div class="mt-3 sm:pe-8">
-            <h3 class="text-lg font-semibold text-gray-900 ">Isi Biodata</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
-            <p class="text-base font-normal text-wrap text-tertiary ">Lengkapi formulir biodata. Setelah disimpan kemudian sistem akan mengirimkan pesan konfirmasi ke email yang didaftarkan.</p>
-        </div>
-    </li>
-    <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
-                <h1 class="text-white">3</h1>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-        </div>
-        <div class="mt-3 sm:pe-8">
-            <h3 class="text-lg font-semibold text-gray-900 ">Pilih Jalur</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
-            <p class="text-base font-normal text-wrap text-tertiary ">Pilih salah satu jalur PPDB yang akan diambil, pastikan hasil scan dokumen persyaratan sudah siap.</p>
-        </div>
-    </li>
-    <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
-                <h1 class="text-white">4</h1>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-        </div>
-        <div class="mt-3 sm:pe-8">
-            <h3 class="text-lg font-semibold text-gray-900 ">Unggah Persyaratan</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
-            <p class="text-base font-normal text-wrap text-tertiary ">Unggah dokumen persyaratan sesuai jalur yang dipilih dengan file .jpg, .jpeg atau .pdf.</p>
-        </div>
-    </li>
-    <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
-                <h1 class="text-white">5</h1>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-        </div>
-        <div class="mt-3 sm:pe-8">
-            <h3 class="text-lg font-semibold text-gray-900 ">Tes Luring</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
-            <p class="text-base font-normal text-wrap text-tertiary ">Mengikuti tes BQ dan akademik secara offline, dengan menaati protokol kesehatan.</p>
-        </div>
-    </li>
-    <li class="sticky mb-6 sm:mb-0 w-full justify-center text-center">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-0 ring-white  sm:ring-8  shrink-0">
-                <h1 class="text-white">6</h1>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-        </div>
-        <div class="mt-3 sm:pe-8">
-            <h3 class="text-lg font-semibold text-gray-900 ">Pengumuman</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released Soon</time>
-            <p class="text-base font-normal text-wrap text-tertiary ">Hasil penerimaan diumumkan sesuai jadwal, melalui website ini dan melalui email yang didaftarkan.</p>
-        </div>
-    </li>
-</ol>
-</div>
-</div>
+    </div>
 </x-app-layout>
