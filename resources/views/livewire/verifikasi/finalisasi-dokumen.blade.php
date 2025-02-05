34bd87640bb86c3250a13f16657676113e32e890
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-02-05 20:21:18 +0700

    fix: nama persyaratan

--- resources/views/livewire/verifikasi/finalisasi-dokumen.blade.php
+++ resources/views/livewire/verifikasi/finalisasi-dokumen.blade.php
@@ -3,11 +3,12 @@
         <div class="mb-4">
             @forelse ($pr->berkas as $berkas)
                 <div class="mb-2 p-4 bg-white shadow rounded">
+                    <h1 class="p-3 font-semibold text-lg text-gray-600">{{ $pr->nama_persyaratan }}</h1>
                     @livewire('pemberkasan.berkas-verif', ['berkas' => $berkas, 'editable' => false], key('berkas-finalisasi-' . $berkas->id))
                 </div>
             @empty
-                <div class="p-4 bg-secondary text-yellow-700 rounded">
-                    <p>Belum ada dokumen {{ $pr->nama_persyaratan }}</p>
+                <div class="mb-2 p-4 bg-white shadow rounded">
+                    <p class="font-semibold text-lg text-red-600">Belum ada dokumen {{ $pr->nama_persyaratan }}</p>
                 </div>
             @endforelse
         </div>
