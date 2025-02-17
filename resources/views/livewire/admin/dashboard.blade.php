b04e14dc52ce8ef9e1be489e85ab3b7175eea8d2
Yazidyah <ahmadyazid579@gmail.com>
2025-02-18 01:05:11 +0700

    FE Colour

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -4,8 +4,8 @@
         @foreach ($statistik->take(5) as $stat)
             <div class="container mx-auto pr-4">
                 <div
-                    class="w-75 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
-                    <div class="h-20 bg-red-400 flex items-center justify-between">
+                    class="w-75  bg-white max-w-xs mx-auto rounded-xl overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform scale-100 hover:scale-110 cursor-pointer">
+                    <div class="h-20 bg-tertiary flex items-center justify-between">
                         <p class="mr-0 text-white text-lg pl-5">{{ $stat->nama_statistik }}</p>
                     </div>
                     <div class="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
@@ -21,26 +21,26 @@
     <!------===================== SECOND ROW CONTAINING THE TABLE STATS STARTS HERE =============================-->
     <div class="flex justify-center bg-gray-100 py-10 p-5">
         <!--==== frist div begins here ====--->
-        <div class="container mr-5 ml-2 mx-auto bg-white shadow-xl">
+        <div class="container mr-5 rounded-lg ml-2 mx-auto bg-white shadow-xl">
             <div class="w-11/12 mx-auto">
                 <div class="bg-white my-6">
                     <table class="text-left w-full border-collapse">
                         <thead>
                             <tr>
                                 <th
-                                    class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">
+                                    class="py-4 px-6 bg-secondary font-bold uppercase text-sm  text-tertiary border-b border-grey-light">
                                     JENIS KELAMIN</th>
                                 <th
-                                    class="py-4 px-6 text-center bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">
+                                    class="py-4 px-6 text-center bg-secondary font-bold uppercase text-sm text-tertiary border-b border-grey-light">
                                     JUMLAH</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter  ">
                                 <td class="py-4 px-6 border-b border-grey-light">LAKI-LAKI</td>
-                                <td class="py-4 px-6 text-center border-b border-grey-light">{{ $countLakiLaki }}</td>
+                                <td class="py-4 px-6 text-center border-b border-grey-light ">{{ $countLakiLaki }}</td>
                             </tr>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter ">
                                 <td class="py-4 px-6 border-b border-grey-light">PEREMPUAN</td>
                                 <td class="py-4 px-6 text-center border-b border-grey-light">{{ $countPerempuan }}</td>
                             </tr>
@@ -52,26 +52,26 @@
         <!--==== frist div ends here ====--->
 
         <!--==== Second div begins here ====--->
-        <div class="container mr-5 mx-auto bg-white shadow-xl">
+        <div class="container mr-5 rounded-lg mx-auto bg-white shadow-xl">
             <div class="w-11/12 mx-auto">
                 <div class="bg-white my-6">
                     <table class="text-left w-full border-collapse">
                         <thead>
                             <tr>
                                 <th
-                                    class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">
+                                    class="py-4 px-6 bg-secondary font-bold uppercase text-sm text-tertiary border-b border-grey-light">
                                     STATUS SEKOLAH</th>
                                 <th
-                                    class="py-4 px-6 text-center bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">
+                                    class="py-4 px-6 text-center bg-secondary font-bold uppercase text-sm text-tertiary border-b border-grey-light">
                                     JUMLAH</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter ">
                                 <td class="py-4 px-6 border-b border-grey-light">NEGERI</td>
                                 <td class="py-4 px-6 text-center border-b border-grey-light">{{ $countSekolahNegeri }}</td>
                             </tr>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter ">
                                 <td class="py-4 px-6 border-b border-grey-light">SWASTA</td>
                                 <td class="py-4 px-6 text-center border-b border-grey-light">{{ $countSekolahSwasta }}</td>
                             </tr>
@@ -82,26 +82,26 @@
         </div>
         <!--==== Second div ends here ====--->
 
-        <div class="container mr-5 mx-auto bg-white shadow-xl">
+        <div class="container mr-5 rounded-lg mx-auto bg-white shadow-xl">
             <div class="w-11/12 mx-auto">
                 <div class="bg-white my-6">
                     <table class="text-left w-full border-collapse">
                         <thead>
                             <tr>
                                 <th
-                                    class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">
+                                    class="py-4 px-6 bg-secondary font-bold uppercase text-sm text-tertiary border-b border-grey-light">
                                     DOMISILI</th>
                                 <th
-                                    class="py-4 px-6 text-center bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">
+                                    class="py-4 px-6 text-center bg-secondary font-bold uppercase text-sm text-tertiary border-b border-grey-light">
                                     JUMLAH</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter ">
                                 <td class="py-4 px-6 border-b border-grey-light">LUAR KOTA</td>
                                 <td class="py-4 px-6 text-center border-b border-grey-light">{{ $countLuarBogor }}</td>
                             </tr>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter ">
                                 <td class="py-4 px-6 border-b border-grey-light">DALAM KOTA</td>
                                 <td class="py-4 px-6 text-center border-b border-grey-light">{{ $countDalamBogor }}</td>
                             </tr>
@@ -119,23 +119,23 @@
                         <thead>
                             <tr>
                                 <th
-                                    class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">
+                                    class="py-4 px-6 bg-secondary font-bold uppercase text-sm text-tertiary border-b border-grey-light">
                                     STATUS</th>
                                 <th
-                                    class="py-4 px-6 text-center bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">
+                                    class="py-4 px-6 text-center bg-secondary font-bold uppercase text-sm text-tertiary border-b border-grey-light">
                                     JUMLAH</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter ">
                                 <td class="py-4 px-6 border-b border-grey-light">BELUM DIKETAHUI</td>
                                 <td class="py-4 text-center px-6 border-b border-grey-light">{{ $countBelumDiproses }}</td>
                             </tr>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter ">
                                 <td class="py-4 px-6 border-b border-grey-light">LULUS</td>
                                 <td class="py-4 text-center px-6 border-b border-grey-light">{{ $countLulus }}</td>
                             </tr>
-                            <tr class="hover:bg-grey-lighter">
+                            <tr class="hover:bg-grey-lighter ">
                                 <td class="py-4 px-6 border-b border-grey-light">TIDAK LULUS</td>
                                 <td class="py-4 text-center px-6 border-b border-grey-light">{{ $countTidakLulus }}</td>
                             </tr>
