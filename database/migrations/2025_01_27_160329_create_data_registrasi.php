2595c9f788b848c65379051aa4fc93f68510d369
Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
2025-02-18 21:26:02 +0700

    refactor: replace individual timestamp fields with timestamps and softDeletes in data_registrasi migration

--- database/migrations/2025_01_27_160329_create_data_registrasi.php
+++ database/migrations/2025_01_27_160329_create_data_registrasi.php
@@ -17,9 +17,8 @@ return new class extends Migration
             $table->unsignedBigInteger('id_jalur')->nullable();
             $table->string('kode_registrasi')->nullable();
             $table->string('status', 20)->default('0')->nullable();
-            $table->timestamp('tanggal_daftar')->useCurrent();
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
+            $table->timestamps(); // created_at and updated_at
+            $table->softDeletes(); // deleted_at
             $table->foreign('id_calon_siswa')->references('id_calon_siswa')->on('calon_siswa')->onDelete('cascade');
         });
     }
