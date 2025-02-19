9cedb83d00e2c43f58b9e46f4fb426b6cc42803c
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-02-19 16:23:10 +0700

    fix: ui

--- resources/views/livewire/admin/data-siswa.blade.php
+++ resources/views/livewire/admin/data-siswa.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="p-4  dark:border-gray-700 mt-14">
+    <div class="p-4  dark:border-gray-700">
         <div class="container mx-auto text-center pt-7">
             <div class="flex justify-between items-left mb-3">
                 <button onclick="window.location.href='{{ route('admin.dashboard') }}'"
