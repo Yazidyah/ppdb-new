0fe9e3eac85f8a3bc4bc972014f907af0bc1cdc1
Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
2025-02-20 23:31:40 +0700

    feat: update navigation links and add status label mapping in OperatorController

--- resources/views/layouts/navigation-op.blade.php
+++ resources/views/layouts/navigation-op.blade.php
@@ -171,14 +171,14 @@ switch ($userRole){
 <aside x-show="cheat" id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full border-r border-gray-200 sm:translate-x-0 " aria-label="Sidebar">
    <div  class="h-full px-3 py-4 overflow-y-auto bg-tertiary">
       <ul  class="space-y-2 font-medium">
-         <li>
-         <x-side-nav :href="route($redirectUrlsyarat)" :active="request()->routeIs($redirectUrlsyarat)">
-         {{ __('Konfigurasi Persyaratan') }}
+      <li>
+         <x-side-nav :href="route($redirectUrls)" :active="request()->routeIs($redirectUrls)">
+         {{ __('Data Pendaftar') }}
          </x-side-nav>
          </li>
          <li>
-         <x-side-nav :href="route($redirectUrls)" :active="request()->routeIs($redirectUrls)">
-         {{ __('Data Pendaftar') }}
+         <x-side-nav :href="route($redirectUrlsyarat)" :active="request()->routeIs($redirectUrlsyarat)">
+         {{ __('Konfigurasi Persyaratan') }}
          </x-side-nav>
          </li>
          <li>
