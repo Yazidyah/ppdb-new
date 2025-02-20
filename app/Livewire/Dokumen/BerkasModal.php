514dea789a051c0ba0acc0afa3dacd634ba0281e
Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
2025-02-20 22:48:47 +0700

    [FEAT] Add modal for data_berkas

--- app/Livewire/Dokumen/BerkasModal.php
+++ app/Livewire/Dokumen/BerkasModal.php
@@ -2,38 +2,49 @@
 
 namespace App\Livewire\Dokumen;
 
-use Illuminate\Support\Facades\Auth;
-use App\Models\DataBerkas;
 use App\Models\Berkas;
+use App\Models\Persyaratan;
 use Livewire\Component;
+use Illuminate\Support\Facades\Log;
 
 class BerkasModal extends Component
 {
+    public $id;
     public $modalSubmit = false;
-    public $dataBerkas;
-    public $berkas;
-    public $dataBerkasId;
-
+    public $berkasId;
+    public $data_berkas;
+    public $isian_berkas;
+    public $syarat;
     protected $listeners = ['openBerkasModal' => 'openModal'];
 
-    public function openModal()
+    public function openModal($id)
     {
+        $this->berkasId = $id;
+        $this->data_berkas = Berkas::find($id);
         $this->modalSubmit = true;
+        $this->syarat = Persyaratan::find($this->data_berkas->id_syarat);
+    }
+
+    public function simpan()
+    {
+        $this->validate([
+            'isian_berkas' => 'required|string',
+        ]);
+
+        if ( $this->data_berkas) {
+            $this->data_berkas->data_berkas = $this->isian_berkas;
+            $this->data_berkas->save();
+            Log::debug('Berkas berhasil disimpan.', ['id' => $this->berkasId]);
+        } else {
+            Log::error('Berkas ID tidak ditemukan saat simpan.', ['id' => $this->berkasId]);
+        }
+        $this->closeModal();
     }
 
     public function closeModal()
     {
         $this->modalSubmit = false;
-        $this->reset(['dataBerkas', 'dataBerkasId']);
-    }
-
-    public function mount(Berkas $berkas)
-    {
-        //mount here
-    }
-    public function update()
-    {
-        //update here
+        $this->reset();
     }
 
     public function render()
