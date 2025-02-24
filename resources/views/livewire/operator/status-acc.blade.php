07adb4d57f642897fd76be8ac7a8e4858fb93ac7
Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
2025-02-24 18:08:51 +0700

    [REFACTOR] Improve modal layout and enhance status update form for better usability

--- resources/views/livewire/operator/status-acc.blade.php
+++ resources/views/livewire/operator/status-acc.blade.php
@@ -1,11 +1,12 @@
 <div>
     <button wire:click="$set('modalOpen', true)" type="button"
-        class="inline-flex items-center justify-center text-white h-10 px-4 py-2 text-sm font-medium transition-colors bg-tertiary border rounded-md hover:bg-secondary hover:text-tertiary active:bg-seconadry focus:bg-seconadry focus:outline-none focus:ring-2 focus:ring-seconadry focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none">
-        Status</button>
+        class="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium text-white transition-colors bg-tertiary border rounded-md hover:bg-secondary hover:text-tertiary active:bg-secondary focus:bg-secondary focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none">
+        Status
+    </button>
 
     @if ($modalOpen)
         <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div class="bg-white p-6 rounded-lg relative">
+            <div class="relative w-3/4 max-w-md p-6 bg-white rounded-lg">
                 <button wire:click="$set('modalOpen', false)"
                     class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 text-red-400 rounded-full hover:text-white hover:bg-red-400">
                     <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -13,17 +14,22 @@
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
-                <h2 class="text-xl font-bold mb-4">Ubah Status</h2>
+                <h2 class="mb-4 text-xl font-bold">Penerimaan {{ ucwords($siswa->nama_lengkap) }}</h2>
+                <hr class="mb-4">
                 <form wire:submit.prevent="simpan">
-                    <select wire:model="status" class="px-4 py-2 border rounded-lg mb-4">
-                        @foreach ($statusList as $key => $value)
-                            <option value="{{ $key }}">{{ $value }}</option>
-                        @endforeach
-                    </select>
+                    <div class="flex items-center mb-4">
+                        <label for="status" class="mr-4">Penerimaan</label>
+                        <select id="status" wire:model="status" class="px-4 py-2 border rounded-lg">
+                            @foreach ($statusList as $key => $value)
+                                <option value="{{ $key }}">{{ $value }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                     <div class="flex justify-end">
                         <button type="button" wire:click="$set('modalOpen', false)"
-                            class="bg-red-900 text-white px-4 py-2 hover:bg-red-500  rounded mr-2">Batal</button>
-                        <button type="submit" class="bg-tertiary text-white px-4 py-2  hover:bg-secondary hover:text-tertiary rounded">Simpan</button>
+                            class="px-4 py-2 mr-2 text-white bg-red-900 rounded hover:bg-red-500">Batal</button>
+                        <button type="submit"
+                            class="px-4 py-2 text-white bg-tertiary rounded hover:bg-secondary hover:text-tertiary">Simpan</button>
                     </div>
                 </form>
             </div>
--- resources/views/livewire/operator/verif-berkas.blade.php
+++ resources/views/livewire/operator/verif-berkas.blade.php
@@ -9,12 +9,12 @@
                 <div class="flex items-center justify-between pb-3">
                     <h3 class="text-lg font-semibold">Verifikasi {{ ucwords($siswa->nama_lengkap) }}</h3>
                     <button wire:click="$set('modalOpen', false)"
-                        class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 text-gray-600 rounded-full hover:text-gray-800 hover:bg-gray-50">
-                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
+                    class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 text-red-400 rounded-full hover:text-white hover:bg-red-400">
+                    <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
                 </div>
                 <div class="relative w-auto pb-8">
                     <input type="hidden" wire:model="id_registrasi">
