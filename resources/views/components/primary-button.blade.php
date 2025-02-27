a2563bce9f6dab15c7043fb425395554560594cc
Ibnuardhians <109910417+ibnardhians@users.noreply.github.com>
2025-02-27 15:34:59 +0700

    ganti isi email (#21)
    Co-authored-by: Yazidyah <ahmadyazid579@gmail.com>

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-primary hover:bg-tertiary border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-tertiary  focus:bg-gray-700 dark:focus:bg-white active:bg-white active:border active:border-tertiary  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-primary hover:bg-tertiary border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-tertiary  focus:bg-tertiary activer:text-white active:bg-tertiary active:border active:border-tertiary  focus:outline-none focus:ring-2 focus:ring-tertiary focus:ring-offset-2  transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<table class="header" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td class="header-content" align="center">
+            <img src="{{ asset('logoman.png') }}" style="max-width: 150px; margin-bottom: 20px;" alt="Logo">
+        </td>
+    </tr>
+</table>
