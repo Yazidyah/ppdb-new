615fad6ab33c3239c803395fb1cb3b6cb439e18e
Ibnuardhians <109910417+ibnardhians@users.noreply.github.com>
2025-02-28 20:14:54 +0700

    Ibnu add sidebar toggle (#23)
    * feat: implement collapsible sidebar navigation and enhance student data display
    
    * fix toggle and layout op
    
    * fix data yang ga muncul
    
    ---------
    
    Co-authored-by: Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
    Co-authored-by: Yazidyah <ahmadyazid579@gmail.com>

--- resources/views/layouts/navigation-op.blade.php
+++ resources/views/layouts/navigation-op.blade.php
@@ -70,6 +70,14 @@ switch ($userRole){
             
                 <!-- Logo -->
                 <div x-data="{cheat:false}" class="flex items-center gap-1 justify-center px-6">
+                <button @click="$store.sidebar.toggle()" class="fixed top-5 left-5 z-50 p-2 bg-white hover:bg-secondary text-tertiary rounded-md shadow-md">
+        <svg x-show="!$store.sidebar.isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+        <svg x-show="$store.sidebar.isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    </button>
                     <a href={{ route($redirectUrl) }}>
                         <x-application-logo class=" h-9 w-auto fill-current text-gray-800 " />
                     </a>
@@ -176,42 +184,54 @@ switch ($userRole){
         </div>
     </div>
 </nav>
-<aside x-show="cheat" id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full border-r border-gray-200 sm:translate-x-0" aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto bg-tertiary">
-        <ul class="space-y-2 font-medium">
+<div x-data="{ isOpen: false }">
+    <!-- Toggle Button -->
+    
+    
+    <!-- Sidebar -->
+    <aside  x-show="$store.sidebar.isOpen" @click.outside="$store.sidebar.isOpen = false" x-show="isOpen" @click.outside="isOpen = false" x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in duration-200 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 border-r border-gray-200 bg-tertiary">
+        <div class="h-full px-3 py-4 overflow-y-auto">
+            <ul class="space-y-2 font-medium">
             <li>
                 <x-side-nav :href="route($redirectUrls)" :active="request()->routeIs($redirectUrls)">
                     {{ __('Data Pendaftar') }}
                 </x-side-nav>
             </li>
-            <li>
-                <x-side-nav :href="route($redirectUrlsyarat)" :active="request()->routeIs($redirectUrlsyarat)">
-                    {{ __('Konfigurasi Persyaratan') }}
-                </x-side-nav>
-            </li>
-            <li>
-                <x-side-nav :href="route($redirectUrljalur)" :active="request()->routeIs($redirectUrljalur)">
-                    {{ __('Konfigurasi Jalur') }}
-                </x-side-nav>
-            </li>
-            <li>
-                <x-side-nav :href="route($redirectUrlpot)" :active="request()->routeIs($redirectUrlpot)">
-                    {{ __('Konfigurasi Pekerjaan Orang Tua') }}
-                </x-side-nav>
-            </li>
-            <li>
-                <x-side-nav :href="route($redirectUrltes)" :active="request()->routeIs($redirectUrltes)">
-                    {{ __('Konfigurasi Jadwal Tes') }}
-                </x-side-nav>
-            </li>
-            <li>
-                <x-side-nav :href="route($redirectUrlls)" :active="request()->routeIs($redirectUrlls)">
-                    {{ __('Data Pendaftar Melaju ke (Step 2)') }}
-                </x-side-nav>
-            </li>
-        </ul>
-    </div>
-</aside>
+                <li>
+                    <x-side-nav :href="route($redirectUrlsyarat)" :active="request()->routeIs($redirectUrlsyarat)">
+                        {{ __('Konfigurasi Persyaratan') }}
+                    </x-side-nav>
+                </li>
+                <li>
+                    <x-side-nav :href="route($redirectUrljalur)" :active="request()->routeIs($redirectUrljalur)">
+                        {{ __('Konfigurasi Jalur') }}
+                    </x-side-nav>
+                </li>
+                <li>
+                    <x-side-nav :href="route($redirectUrlpot)" :active="request()->routeIs($redirectUrlpot)">
+                        {{ __('Konfigurasi Pekerjaan Orang Tua') }}
+                    </x-side-nav>
+                </li>
+                <li>
+                    <x-side-nav :href="route($redirectUrltes)" :active="request()->routeIs($redirectUrltes)">
+                        {{ __('Konfigurasi Jadwal Tes') }}
+                    </x-side-nav>
+                </li>
+                <li>
+                    <x-side-nav :href="route($redirectUrlls)" :active="request()->routeIs($redirectUrlls)">
+                        {{ __('Data Pendaftar Melaju ke (Step 2)') }}
+                    </x-side-nav>
+                </li>
+            </ul>
+        </div>
+    </aside>
+</div>
+
+<main>
+    @yield('content')
+</main>
+@livewireScripts
+
 
 <main>
     @yield('content')
