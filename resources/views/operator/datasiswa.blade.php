5e1dc9a9d7d92c34b1526cdf003cc511b71fe1c4
Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
2025-02-28 20:14:05 +0700

    fix data yang ga muncul

--- resources/views/operator/datasiswa.blade.php
+++ resources/views/operator/datasiswa.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-<div x-data class="p-4 transition-all duration-300" x-bind:class="$store.sidebar.isOpen ? 'sm:ml-64' : ' ml-0'">
+<div x-data="{ tahun: true }" class="p-4 transition-all duration-300" x-bind:class="$store.sidebar.isOpen ? 'sm:ml-64' : ' ml-0'">
     <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
             <div class="container mx-auto text-center pt-7">
                 <div class="container mx-auto text-center pt-7 ">
