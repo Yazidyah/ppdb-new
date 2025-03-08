bf40d0b40a09d8b0b93b77023757673d6796a5e7
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-03-08 14:47:48 +0700

    fix: truncate

--- resources/views/livewire/pemberkasan/berkas.blade.php
+++ resources/views/livewire/pemberkasan/berkas.blade.php
@@ -10,7 +10,9 @@
                 <div class="flex items-start">
                     <div class="flex flex-col text-left">
                         <div class="flex items-center text-sm">
-                            <div class="font-medium">{{ $berkas->original_name }}</div>
+                            <div class="font-medium truncate max-w-sm"> <!-- Adjusted max-width -->
+                                {{ $berkas->original_name }}
+                            </div>
                         </div>
                         <div class="flex items-center text-xs text-gray-500">
                             <div class="font-regular">{{ $berkas->created_at }}</div>
@@ -25,7 +27,6 @@
                         stroke="currentColor" class="w-5 h-5">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                        \n"
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
                 </button>
@@ -37,7 +38,7 @@
                     </svg>
                 </button>
                 @if ($editable)
-                    <button class="transition transform hover:scale-110 " wire:click="delete">
+                    <button class="transition transform hover:scale-110" wire:click="delete">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-5 h-5 text-primary-600">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -45,7 +46,6 @@
                         </svg>
                     </button>
                 @endif
-
             </div>
         </div>
     </div>
@@ -58,7 +58,7 @@
                     @if ($url)
                         @if (Str::endsWith($berkas->original_name, '.pdf'))
                             <iframe src="{{ $url }}" frameborder="0"
-                                class="w-full h-[70vh] sm:h-[80vh] over pt-2 pb-5 transform transition-all duration-300"
+                                class="w-full h-[70vh] sm:h-[80vh] pt-2 pb-5 transform transition-all duration-300"
                                 style="object-fit: cover;"></iframe>
                         @else
                             <img src="{{ $url }}" alt="Uploaded File" loading="lazy"
@@ -82,6 +82,12 @@
             display: block;
             margin: 0 auto;
         }
-    </style>
 
+        .truncate {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            max-width: 100px;
+        }
+    </style>
 </div>
