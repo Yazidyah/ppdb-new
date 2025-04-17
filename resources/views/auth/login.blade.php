94b22795482bc8ba927d901a2898c8179e25be96
Ibnuardhians <109910417+ibnardhians@users.noreply.github.com>
2025-04-17 20:13:36 +0700

    Ibnu add feature regist verif email (#25)
    * [FEAT] Nambah fitur verifikasi email
    
    * [FIX] routing redirect
    
    * revert a custom email
    
    * FE Tipis
    
    * fix: add missing newline at end of file in web.php
    
    * add name to home route
    
    * [FEAT] Nambah fitur verifikasi email
    
    * [FIX] routing redirect
    
    * revert a custom email
    
    * FE Tipis
    
    * fix: add missing newline at end of file in web.php
    
    * add name to home route
    
    ---------
    
    Co-authored-by: Muhammad Ibnu Ardhiansyah <ibnuardhian@gmail.com>
    Co-authored-by: Yazidyah <ahmadyazid579@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,8 +26,8 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded bg-tertiary border-gray-300  text-tertiary shadow-sm focus:ring-tertiary " name="remember">
-                <span class="ms-2 text-sm text-gray-600 ">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300  text-tertiary shadow-sm focus:ring-tertiary " name="remember">
+                <span class="ms-2 text-sm text-gray-600 ">{{ __('Ingat Saya') }}</span>
             </label>
         </div>
         
