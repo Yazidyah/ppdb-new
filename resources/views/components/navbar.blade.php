f6c73abfa20d1667fcba96da880847901f9e2c7a
Yazidyah <ahmadyazid579@gmail.com>
2025-04-29 19:41:49 +0700

    ilangin register

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -70,8 +70,8 @@
                 <div class="flex justify-between gap-4 px-4 pb-4 mt-4">
                     <a href="login"
                         class="text-white outline-secondary outline px-7 flex items-center justify-center rounded-full font-bold text-xs xl:text-base hover:bg-white hover:text-primary">Masuk</a>
-                    <a href="register"
-                        class="flex items-center justify-center w-full text-sm font-bold px-9 py-4 bg-white rounded-full hover:bg-secondary hover:text-primary text-primary">Daftar</a>
+                    <!-- <a href="register"
+                        class="flex items-center justify-center w-full text-sm font-bold px-9 py-4 bg-white rounded-full hover:bg-secondary hover:text-primary text-primary">Daftar</a> -->
                 </div>
                 @endif
         </div>
