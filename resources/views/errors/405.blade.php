e2183ee51e8fe6d3b51c8ba761647dde1fda7bb9
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-04-29 11:33:42 +0700

    fix

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.errors')
 
-@section('title', __('Method Not Allowed'))
+@section('title', 'Oops! Terjadi Kesalahan')
 @section('code', '405')
 @section('message',
     'Maaf, sepertinya ada sesuatu yang salah di sisi kami. Tim kami telah diberitahu dan sedang bekerja untuk memperbaiki
