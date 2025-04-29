bb474bb2239aa43f9ae1d1696594ee771052186f
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-04-29 11:30:52 +0700

    405 error

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.errors')
+
+@section('title', __('Method Not Allowed'))
+@section('code', '405')
+@section('message',
+    'Maaf, sepertinya ada sesuatu yang salah di sisi kami. Tim kami telah diberitahu dan sedang bekerja untuk memperbaiki
+    masalah ini secepat mungkin.
+
+    ')
