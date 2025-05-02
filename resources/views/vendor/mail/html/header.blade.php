d7649a329b53c7aa8548852d21afb95827c74c8d
Yazidyah <ahmadyazid579@gmail.com>
2025-05-02 17:09:58 +0700

    header email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <table class="header" width="100%" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
             <td align="center" style="padding: 20px; background-color: #d1fae5;">
-                <img src="https://man1kotabogor.sch.id/uploads/frontend/images/logo1.png" alt="Logo MAN 1 Bogor" style="max-width: 80px; display: block; margin-bottom: 10px;">
+                <img src="ppdb.man1kotabogor.id/logoman.png" alt="Logo MAN 1 Bogor" style="max-width: 80px; display: block; margin-bottom: 10px;">
                 <div style="font-size: 20px; font-weight: bold; color: #065f46;">MAN 1 Kota Bogor</div>
                 <div style="font-size: 14px; color: #065f46;">Komplek Bumi Meteng Asri Jl. Terapi Raya.11a, Parung Jambu RT No.02, RW.11, Menteng, Kec. Bogor Bar., Kota Bogor, Jawa Barat 16112</div>
             </td>
