01ccfc21723a2f36817685ee71aa58d4244054c1
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-05-03 11:47:58 +0700

    fix: upload berkas operator

--- app/Livewire/Operator/TabBerkasSiswa.php
+++ app/Livewire/Operator/TabBerkasSiswa.php
@@ -52,7 +52,13 @@ class TabBerkasSiswa extends Component
     {
         $this->syarat = Persyaratan::find($id);
         if ($this->syarat->id_jalur == 1) {
-            $this->kb = KategoriBerkas::where('nama', $this->syarat->nama_persyaratan)->where('key', 'jalur_reguler')->first();
+            $this->kb = KategoriBerkas::where('nama', 'ilike', '%' . $this->syarat->nama_persyaratan . '%')->where('key', 'jalur_reguler')->first();
+        }
+        if ($this->syarat->id_jalur == 2) {
+            $this->kb = KategoriBerkas::where('nama', 'ilike', '%' . $this->syarat->nama_persyaratan . '%')->where('key', 'jalur_reguler')->first();
+        }
+        if ($this->syarat->id_jalur == 3) {
+            $this->kb = KategoriBerkas::where('nama', 'ilike', '%' . $this->syarat->nama_persyaratan . '%')->where('key', 'jalur_afirmasi_ketm')->first();
         }
     }
 
