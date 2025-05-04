91e66044a0533e5400c52625b05043009acd9f8a
Yazidyah <ahmadyazid579@gmail.com>
2025-05-04 19:26:03 +0700

    Add Isi Data

--- app/Helpers/DocumentHelper.php
+++ app/Helpers/DocumentHelper.php
@@ -6,7 +6,7 @@ class DocumentHelper
 {
     public static function isSimpleSyarat($namaSyarat) // syarat ga butuh ngisi data
     {
-        $syaratKeyword = ['foto', 'pas foto', 'akta kelahiran', 'rapot', 'raport', 'rapor'];
+        $syaratKeyword = ['foto', 'pas foto', 'akta kelahiran', 'rapot', 'raport', 'rapor', 'Sertifikat', 'KIP/KKS/PKH', 'Tabungan'];
         foreach ($syaratKeyword as $keyword) {
             if (stripos($namaSyarat, $keyword) !== false) {
                 return true;
