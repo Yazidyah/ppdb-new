f80e10be53023b27bd49393f17486f0fde93a5a8
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-05-08 23:52:40 +0700

    fix

--- resources/views/livewire/dokumen/upload-dokumen.blade.php
+++ resources/views/livewire/dokumen/upload-dokumen.blade.php
@@ -315,12 +315,12 @@
             class="px-3 w-full py-1 sm:px-6 sm:py-2 flex items-center justify-center hover:bg-secondary rounded-xl text-secondary font-medium bg-tertiary hover:text-tertiary"
             type="button" id="submitBtn">Lanjutkan ke tahap verifikasi</button> -->
         @if ($isRapotLengkap == true)
-            <button wire:click="validateAndSubmit"
+            <button onclick="window.location.href='/siswa/daftar-step-empat?t=1'"
                 class="px-3 w-full py-1 sm:px-6 sm:py-2 flex items-center justify-center hover:bg-secondary rounded-xl text-secondary font-medium bg-tertiary hover:text-tertiary"
                 type="button" id="submitBtn">Lanjutkan ke tahap verifikasi</button>
         @endif
         @if ($isRapotLengkap == false)
-            <button wire:click="validateAndSubmit"
+            <button onclick="window.location.href='/siswa/daftar-step-empat?t=1'"
                 class="px-3 w-full py-1 sm:px-6 sm:py-2 flex items-center justify-center hover:bg-secondary rounded-xl text-secondary font-medium bg-tertiary hover:text-tertiary cursor-not-allowed relative group"
                 type="button" id="submitBtn" disabled>
                 Lanjutkan ke tahap verifikasi
