7d10c214bc1d9077b510721fd3a7bc7e422a07e7
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-05-15 11:46:16 +0700

    fix: security

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -29,24 +32,41 @@ class AuthenticatedSessionController extends Controller
             'password' => ['required', 'string'],
         ]);
 
+        $throttleKey = Str::lower($request->input('email')) . '|' . $request->ip();
+
+        // cek percobaan login bia ga kena brute force
+        if (RateLimiter::tooManyAttempts($throttleKey, 3)) {
+            $seconds = RateLimiter::availableIn($throttleKey);
+
+            return back()->withErrors([
+                'email' => "Terlalu banyak percobaan login. Silakan coba lagi dalam $seconds detik.",
+            ])->withInput();
+        }
+
         $remember = $request->has('remember');
 
         if (Auth::attempt($request->only('email', 'password'), $remember)) {
+            // kalo udah login, ga usah di rate limit lagi
+            RateLimiter::clear($throttleKey);
+
             $request->session()->regenerate();
 
             $loggedInUserRole = $request->user()->role;
 
-            if ($loggedInUserRole == 'admin'){
+            if ($loggedInUserRole == 'admin') {
                 return redirect()->intended(route('admin.dashboard', absolute: false));
-            } elseif ($loggedInUserRole == 'operator'){
+            } elseif ($loggedInUserRole == 'operator') {
                 return redirect()->intended(route('operator.dashboard', absolute: false));
             }
             return redirect()->intended(route('siswa.dashboard', absolute: false));
         }
 
+        // hit berapa kali percobaan gagal
+        RateLimiter::hit($throttleKey);
+
         return back()->withErrors([
-            'email' => __('The provided credentials do not match our records.'),
-        ]);
+            'email' => __('Email atau password tidak sesuai.'),
+        ])->withInput();
     }
 
     /**
