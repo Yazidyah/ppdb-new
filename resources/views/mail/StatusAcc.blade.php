d52fa207075670b660019a8c50e9ed40432086a9
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-05-21 14:57:46 +0700

    fix: email

--- resources/views/mail/StatusAcc.blade.php
+++ resources/views/mail/StatusAcc.blade.php
@@ -7,8 +7,8 @@ Hai **{{ $name }}**,
 Kami dengan hormat ingin menginformasikan hasil seleksi Penerimaan Peserta Didik Baru (PPDB) MAN 1 Kota Bogor.
 @component('mail::subcopy')
 @switch($status)
-    @case('8')
-        Kami dengan bangga mengumumkan bahwa Anda telah **DITERIMA** sebagai siswa baru di MAN 1 Kota Bogor. 
+    @case('7')
+        Kami dengan bangga mengumumkan bahwa Anda telah **DITERIMA** sebagai siswa baru di MAN 1 Kota Bogor.
         Silakan segera melakukan proses daftar ulang sesuai dengan informasi yang terlampir.<br>
         Langkah Selanjutnya:<br>
         - Silakan periksa lampiran untuk **Surat Keterangan Kelulusan**.<br>
@@ -16,20 +16,22 @@ Kami dengan hormat ingin menginformasikan hasil seleksi Penerimaan Peserta Didik
         - Jika ada pertanyaan, silakan hubungi panitia PPDB.
     @break
 
-    @case('9') 
+    @case('8')
         Anda telah masuk dalam daftar **CADANGAN**. Jika ada kursi yang tersedia, kami akan menghubungi Anda untuk proses selanjutnya.
     @break
 
-    @default
+    @case('6')
         Kami mohon maaf, Anda **TIDAK DITERIMA** dalam seleksi kali ini. Terima kasih telah berpartisipasi, tetap semangat dan jangan berhenti untuk mengejar mimpi Anda!
+    @break
+    @default
 @endswitch
 @endcomponent
 
 
 Terima kasih atas partisipasi Anda dalam proses seleksi ini. Kami mendoakan yang terbaik untuk masa depan Anda.
 
-Hormat kami,  
-**Panitia PPDB MAN 1 Kota Bogor**  
+Hormat kami,
+**Panitia PPDB MAN 1 Kota Bogor**
 **{{ config('app.name') }}**
 
 @endcomponent
