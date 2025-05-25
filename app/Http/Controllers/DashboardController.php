b27e5342e5214e1b41cc93ec53fb1168b7f5ebbd
Yazidyah <ahmadyazid579@gmail.com>
2025-05-26 01:47:06 +0700

    nambah di Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,11 +10,20 @@ class DashboardController extends Controller
         $totalPendaftar = DataRegistrasi::where('status','>=',3)->count();
         $pendaftarSudahDiproses = DataRegistrasi::where('status', '>', 3)->count();
         $pendaftarHarusDiproses = DataRegistrasi::where('status', 3)->count();
+        
+        $totalPendaftarReguler = DataRegistrasi::where('status','>=',3)->where('id_jalur',1)->count();
+        $pendaftarSudahDiprosesReguler = DataRegistrasi::where('status', '>', 3)->where('id_jalur',1)->count();
+        $pendaftarHarusDiprosesReguler = DataRegistrasi::where('status', 3)->where('id_jalur',1)->count();
 
         // Calculate the percentage
         $persentaseSudahDiproses = $totalPendaftar > 0 
             ? round(($pendaftarSudahDiproses / $totalPendaftar) * 100) 
             : 0;
-        return view('operator.dashboard', compact('pendaftarHarusDiproses', 'pendaftarSudahDiproses', 'persentaseSudahDiproses'));
+        $persentaseSudahDiprosesReguler = $totalPendaftarReguler > 0 
+            ? round(($pendaftarSudahDiprosesReguler / $totalPendaftarReguler) * 100) 
+            : 0;
+        return view('operator.dashboard', compact('pendaftarHarusDiproses', 'pendaftarSudahDiproses', 'persentaseSudahDiproses',
+            'pendaftarHarusDiprosesReguler', 'pendaftarSudahDiprosesReguler', 'persentaseSudahDiprosesReguler',
+            'totalPendaftar', 'totalPendaftarReguler'));
     }
 }
