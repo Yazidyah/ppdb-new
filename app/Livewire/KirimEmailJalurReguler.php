348d6a6a52f0268eb0ee0610d507dba0e272fafe
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-06-26 23:44:48 +0700

    fix: improve email sending logic in KirimEmailJalurReguler

--- app/Livewire/KirimEmailJalurReguler.php
+++ app/Livewire/KirimEmailJalurReguler.php
@@ -20,21 +20,25 @@ class KirimEmailJalurReguler extends Component
 
     public function kirimEmail()
     {
-        foreach ($this->siswa->sortBy('id_calon_siswa') as $s) {
+        foreach ($this->siswa->whereIn('id_calon_siswa', [855, 854, 853, 852, 851, 850, 849, 848, 847, 846, 845, 844, 843, 842, 841, 840, 839, 838, 837, 836, 835, 834, 833, 832, 831, 830, 829, 828, 827, 826, 825, 824, 823, 822, 821, 820, 819, 818, 817, 816, 815, 814, 813, 812, 811, 810, 809, 808, 807, 806, 805, 804, 803, 802, 801])->sortBy('id_calon_siswa') as $s) {
             if ($s->dataRegistrasi == null) {
                 continue;
             } else {
                 if ($s->dataRegistrasi->id_jalur == 1) {
                     if ($s->dataRegistrasi->status == 6) {
-                        $messageBody = $s->dataRegistrasi->status === '8'
-                            ? "Kamu dicadangkan."
-                            : ($s->dataRegistrasi->status === '7'
-                                ? "Selamat, Kamu telah diterima."
-                                : ($s->dataRegistrasi->status === '6'
-                                    ? "Maaf, Kamu tidak diterima."
-                                    : "Status kamu belum diproses."));
-                        SendStatusAccEmail::dispatch($s, $messageBody, $s->dataRegistrasi->status);
-                        Log::info('Email sent to: ' . $s->user->email . ' with status: ' . $s->dataRegistrasi->status);
+                        if ($s->user->email !== null) {
+                            $messageBody = $s->dataRegistrasi->status === '8'
+                                ? "Kamu dicadangkan."
+                                : ($s->dataRegistrasi->status === '7'
+                                    ? "Selamat, Kamu telah diterima."
+                                    : ($s->dataRegistrasi->status === '6'
+                                        ? "Maaf, Kamu tidak diterima."
+                                        : "Status kamu belum diproses."));
+                            SendStatusAccEmail::dispatch($s, $messageBody, $s->dataRegistrasi->status);
+                            Log::info('Email sent to: ' . $s->user->email . ' with status: ' . $s->dataRegistrasi->status);
+                        } else {
+                            continue;
+                        }
                     } else {
                         continue;
                     }
