7b1c7f6c2cc062c89e713ee2711278f6770669e1
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2026-04-19 19:18:22 +0700

    test ci cd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="container mx-auto pt-5 px-4">
         <div class="my-4 bg-tertiary rounded-lg text-white text-center py-8 leading-tight">
-            <h2 class="font-bold text-3xl md:text-4xl ">Selamat Datang di Website PPDB MAN 1 Kota Bogor</h2>
+            <h2 class="font-bold text-3xl md:text-4xl ">Selamat Datang di Website PPDB MAN 1 Kota Bogorrrr</h2>
         </div>
     </div>
     @livewire('home.countdown-jalur')
